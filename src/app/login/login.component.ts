3ae57460c586d22da0e08b9bab609f16b48fc72b
nareshkumar-h <nareshkumarh@live.com>
2024-09-26 12:26:45 +0530

    Added registration functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -22,9 +22,21 @@ export class LoginComponent {
     console.log('Form Submitted');
     console.log('Email:', this.email, ',Password:' + this.password);
 
+    //Backend API url ( data: email,password )
     //if admin@gmail.com, pass123 , valid login
 
-    if (this.email == "admin@gmail.com" && this.password == "pass123") {
+    // if (this.email == "admin@gmail.com" && this.password == "pass123") {
+
+
+    // 1. Fetch the registered user details from localStorage
+    const usersStr = localStorage.getItem("USERS"); // returns in string
+    const users = usersStr != null ? JSON.parse(usersStr) : []; // convert string to object 
+
+    //Using find check the input email/password matches
+    //axios.post(url, data).then(res=> { ... })
+    const userExists = users.find((obj: any) => obj.email == this.email && obj.password == this.password);
+
+    if (userExists != null) {
       alert("Successfully Loggedin");
       localStorage.setItem("LOGGED_IN_USER", "true");
       localStorage.setItem("EMAIL", this.email);
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,48 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.component.html',
   styles: ``
 })
 export class RegisterComponent {
+
+  name!: string;
+  email!: string;
+  password!: string;
+
+  constructor() {
+    console.log('RegisterComponent constructor called');
+  }
+
+  onSubmit() {
+    console.log('Form Submitted');
+    console.log('Name:', this.name + ",email:", this.email + ",password:" + this.password);
+
+    const userObj = {
+      name: this.name,
+      email: this.email,
+      password: this.password
+    };
+
+    console.log('UserObj:', userObj);
+
+    //1. send userObj to backend REST API url => insert the user details into database
+    //axios.post(url, userObj).then (res=> ...)
+
+    //2. Temporarily storing users data in localStorage
+    const users = [];
+    users.push(userObj);
+    localStorage.setItem("USERS", JSON.stringify(users));
+
+    alert("Successfully Registered");
+
+    window.location.href = "/login";
+
+  }
 
 }
